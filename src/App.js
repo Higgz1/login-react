756ad1e80a340278ffb967a84f92f46cd53c01c9
Bryan Higgz <bryan.kahiga@gmail.com>
2022-01-28 10:43:15 +0300

    added route to new page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-import { UserProvider } from './context/UserContext';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LoginForm from './components/LoginForm/LoginForm';
+import "./App.css";
+import { UserProvider } from "./context/UserContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginForm from "./components/LoginForm/LoginForm";
+import ContentPage from "./pages/ContentPage";
 
 function App() {
   return (
     <UserProvider>
       <Router>
-        <LoginForm />
+        <Routes>
+          <Route path="/" element={<LoginForm />}></Route>
+          <Route path="/content" element={<ContentPage />}></Route>
+        </Routes>
       </Router>
     </UserProvider>
   );
